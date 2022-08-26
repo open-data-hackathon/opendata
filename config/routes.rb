7aaac15027a0d3ab2d11a8c54d7025858357e8c2
e601201 <k.arthur1111@gmail.com>
2022-08-27 00:44:33 +0900

    fuga

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "statics#welcome"
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, except: :index
+
+  resources :events do
+    resource :favorites, only: %i[create destroy]
+    resource :preview, only: :show
+    resources :comments, only: %i[create destroy], shallow: true
+  end
+  resource :profile, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update] 
 end
