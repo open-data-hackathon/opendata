071df4e88d96d1f704ff1d40cd1816272c1da46b
kaoritorikai23 <kaori.torikai@gmail.com>
2022-08-28 00:05:38 +0900

    ヘッダにパスを記載、イベントビュー編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_031633) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_085025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "events_image"
+    t.datetime "date"
+    t.integer "member"
+    t.integer "state"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_031633) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
