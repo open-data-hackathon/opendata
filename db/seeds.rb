9e423bc3dde9e4424462e5a076fcd4fab87e5010
e601201 <k.arthur1111@gmail.com>
2022-08-27 20:34:54 +0900

    [test]seed_fileの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Place.destroy_all
+Event.destroy_all
+
+User.create(
+  name: 'test_user',
+  email: 'test@example.com'
+)
+user = User.first
+
+place = Place.create(
+  place_name: '地域活動センター',
+  address: 'どこかの地域',
+  latitude: 123.456,
+  longitude: 123.456
+)
+
+user.events.create(
+  title: "フットサル大会",
+  body: "フットサルをみんなでしましょう！！",
+  place_id: place.id
+)
+
