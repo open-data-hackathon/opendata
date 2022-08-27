132df631687733a38bd79eca76e7bf85dc3aa8c0
yuki_sdy <couragewing.14@gmail.com>
2022-08-27 17:20:56 +0900

    [test]

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+=======
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_071920) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.string "events_image"
+    t.datetime "date", null: false
+    t.integer "member"
+    t.integer "state", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "events", "users"
+>>>>>>> Stashed changes
 end
