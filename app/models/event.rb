d909496f1eb0f17a8824689487fecc60334f18ed
yuki_sdy <couragewing.14@gmail.com>
2022-08-28 13:14:01 +0900

    conflict解消

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
+  belongs_to :place
+  has_many :join_events, dependent: :destroy
+  has_many :joined_users, through: :join_events, source: :user
   enum state: { open: 0, close: 1 }
 end
