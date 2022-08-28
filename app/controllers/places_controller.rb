d235e91185fe38199289c235ddf5d4b38ab53d1a
e601201 <k.arthur1111@gmail.com>
2022-08-28 20:11:37 +0900

    [lint]controllerのリントチェック

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :set_place, only: %i[ show edit update destroy ]
+  before_action :set_place, only: %i[show edit update destroy]
 
   # GET /places
   def index
@@ -7,8 +7,7 @@ class PlacesController < ApplicationController
   end
 
   # GET /places/1
-  def show
-  end
+  def show; end
 
   # GET /places/new
   def new
@@ -16,15 +15,14 @@ class PlacesController < ApplicationController
   end
 
   # GET /places/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /places
   def create
     @place = Place.new(place_params)
 
     if @place.save
-      redirect_to @place, notice: "Place was successfully created."
+      redirect_to @place, notice: 'Place was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +31,7 @@ class PlacesController < ApplicationController
   # PATCH/PUT /places/1
   def update
     if @place.update(place_params)
-      redirect_to @place, notice: "Place was successfully updated."
+      redirect_to @place, notice: 'Place was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +40,18 @@ class PlacesController < ApplicationController
   # DELETE /places/1
   def destroy
     @place.destroy
-    redirect_to places_url, notice: "Place was successfully destroyed."
+    redirect_to places_url, notice: 'Place was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_place
-      @place = Place.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def place_params
-      params.require(:place).permit(:place_name, :address, :latitude, :longitude)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def place_params
+    params.require(:place).permit(:place_name, :address, :latitude, :longitude)
+  end
 end
