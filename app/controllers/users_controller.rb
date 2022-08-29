a25660639beed17f5eaeccc6853962a315c1527e
e601201 <k.arthur1111@gmail.com>
2022-08-29 15:54:03 +0900

    [style]リントチェック

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
   skip_before_action :require_login, only: %i[new create]
 
   # GET /users/1
