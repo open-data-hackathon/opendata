dc493d23bb4804160b71fe2626afa8de3d14d87b
yuki_sdy <couragewing.14@gmail.com>
2022-09-14 18:23:38 +0900

    Fix: @joined_eventsに訂正

--- app/controllers/user/joined_events_controller.rb
+++ app/controllers/user/joined_events_controller.rb
@@ -1,5 +1,5 @@
 class User::JoinedEventsController < ApplicationController
   def index
-    @events = current_user.joined_events.order(created_at: :desc).page(params[:page])
+    @joined_events = current_user.joined_events.order(created_at: :desc).page(params[:page])
   end
 end
