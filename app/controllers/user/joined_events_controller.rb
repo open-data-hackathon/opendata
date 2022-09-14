e2d96ca7b1686a10b9b6924e6c823a83e4a2be08
yuki_sdy <couragewing.14@gmail.com>
2022-09-14 17:21:24 +0900

    Fix: 参加イベント一覧(joined_events)の編集

--- app/controllers/user/joined_events_controller.rb
+++ app/controllers/user/joined_events_controller.rb
@@ -1,5 +1,5 @@
 class User::JoinedEventsController < ApplicationController
   def index
-    @joined_events = current_user.joined_events
+    @events = current_user.joined_events.order(created_at: :desc).page(params[:page])
   end
 end
